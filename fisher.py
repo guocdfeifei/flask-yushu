765796a4767a5e3d2f3fc3236f090b2deacae341
guoff <guocdfeifei@163.com>
2018-12-17 09:22:50 +0800

    运行优化

--- fisher.py
+++ fisher.py
@@ -1,4 +1,5 @@
 from app import create_app
 app=create_app()
 if __name__=='__main__':
-    app.run(host=app.config['HOST'] , port=app.config['PORT'] , debug=app.config['DEBUG'],threaded=False)
+    # host = app.config['HOST'], port = app.config['PORT'],
+    app.run( debug=app.config['DEBUG'],threaded=False)
